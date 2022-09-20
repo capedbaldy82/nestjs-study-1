68c5f7ef3279a853386e7e1768397a19c5fa51e9
chango <kimtothechang@gmail.com>
2022-09-20 23:21:06 +0900

    cret: initial env

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardModule } from './board/board.module';
+import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardModule, AuthModule],
+})
+export class AppModule {}
